61c0f83d1b891dca7b145ebf47fb68026963aac9
devshisir <contact.shisirimran@gmail.com>
2024-01-10 23:46:56 +0600

    first commit

---
+++ asset/js/custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  var $root = $("html, body");
+
+  $('.headerNavArea a[href^="#"]').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          500,
+          "linear"
+        );
+        return false;
+      }
+    }
+  });
+});
